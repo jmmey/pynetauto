94e29882c34c0b125304028fe904d74520e146b8
John Meyers <46431837+jmmey@users.noreply.github.com>
2021-01-27 15:04:23 -0800

    Added these for testing with week two videos

---
+++ week_02/exercise_03.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+from netmiko import ConnectHandler
+from getpass import getpass
+
+my_user = input('Username: ') 
+my_pass = getpass()
+
+# Device Dictionaries
+cisco3 = {
+    'device_type': 'cisco_ios',
+    'host': 'cisco3.lasthop.io',
+    'username': my_user, 
+    'password': my_pass,
+    'session_log': 'cisco3_session.txt'
+    }
+
+cisco4 = {
+    'device_type': 'cisco_ios',
+    'host': 'cisco4.lasthop.io',
+    'username': my_user, 
+    'password': my_pass
+    }
+
+nxos1 = {
+    'device_type': 'cisco_nxos',
+    'host': 'nxos1.lasthop.io',
+    'username': my_user,
+    'password': my_pass
+    }
+
+nxos2 = {
+    'device_type': 'cisco_nxos',
+    'host': 'nxos2.lasthop.io',
+    'username': my_user,
+    'password': my_pass
+    }
+
+ios_devices = [cisco3, cisco3]
+command = 'delete flash:/john_test*.txt'
+
+for device in ios_devices:
+    net_connect = ConnectHandler(**device)
+    net_connect.send_command_timing(command)
+    net_connect.send_command_timing('\n')
+    net_connect.send_command_timing('y')
+    # Graceful Disconnect
+    net_connect.disconnect()
