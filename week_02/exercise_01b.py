0913188668d20a7e70fb12eb02f20fd86910da44
John Meyers <46431837+jmmey@users.noreply.github.com>
2021-02-03 18:33:39 -0800

    Updated to simply login

--- week_02/exercise_01b.py
+++ week_02/exercise_01b.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
+import os
 from netmiko import ConnectHandler
 from getpass import getpass
 from pprint import pprint
 
-my_user = input('Username: ') 
-my_pass = getpass()
+# my_user = input('Username: ') 
+my_user = 'pyclass'
+# my_pass = getpass()
+my_pass = os.getenv('PYNET_PASSWORD') if os.getenv('PYNET_PASSWORD') else getpass()
 
 # Device Dictionaries
 cisco3 = {
@@ -42,14 +45,14 @@ net_connect = ConnectHandler(**cisco4)
 
 command = 'ping'
 
-output = net_connect.send_command(command, expect_string=r':')
-output += net_connect.send_command('\n', expect_string=r':')
-output += net_connect.send_command('8.8.8.8', expect_string=r':')
-output += net_connect.send_command('\n', expect_string=r':')
-output += net_connect.send_command('\n', expect_string=r':')
-output += net_connect.send_command('\n', expect_string=r':')
-output += net_connect.send_command('\n', expect_string=r':')
-output += net_connect.send_command('\n', expect_string=r'#')
+output = net_connect.send_command(command, expect_string=r':', strip_prompt=False, strip_command=False)
+output += net_connect.send_command('\n', expect_string=r':', strip_prompt=False, strip_command=False)
+output += net_connect.send_command('8.8.8.8', expect_string=r':', strip_prompt=False, strip_command=False)
+output += net_connect.send_command('\n', expect_string=r':', strip_prompt=False, strip_command=False)
+output += net_connect.send_command('\n', expect_string=r':', strip_prompt=False, strip_command=False)
+output += net_connect.send_command('\n', expect_string=r':', strip_prompt=False, strip_command=False)
+output += net_connect.send_command('\n', expect_string=r':', strip_prompt=False, strip_command=False)
+output += net_connect.send_command('\n', expect_string=r'#', strip_prompt=False, strip_command=False)
 
 # output = net_connect.send_command_timing('ping')
 # output += net_connect.send_command_timing('\n')
@@ -60,7 +63,9 @@ output += net_connect.send_command('\n', expect_string=r'#')
 # output += net_connect.send_command_timing('\n')
 # output += net_connect.send_command_timing('\n')
 
-pprint(output)
-
-# Graceful Disconnect
 net_connect.disconnect()
+
+print()
+print(output)
+print()
+
