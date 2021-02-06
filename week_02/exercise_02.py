0494a51d01a691c584d51fa2703470433425aae2
John Meyers <46431837+jmmey@users.noreply.github.com>
2021-02-06 06:52:44 -0800

    Week 2 exercise 2

---
+++ week_02/exercise_02.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import os
+from netmiko import ConnectHandler
+from getpass import getpass
+from pprint import pprint
+
+my_user = 'pyclass'
+my_pass = os.getenv('PYNET_PASSWORD') if os.getenv('PYNET_PASSWORD') else getpass()
+
+# Device Dictionaries
+cisco3 = {
+    'device_type': 'cisco_ios',
+    'host': 'cisco3.lasthop.io',
+    'username': my_user, 
+    'password': my_pass,
+    'session_log': 'cisco3_session.txt'
+    }
+
+cisco4 = {
+    'device_type': 'cisco_ios',
+    'host': 'cisco4.lasthop.io',
+    'username': my_user, 
+    'password': my_pass,
+    'session_log': 'cisco4_session.txt'
+    }
+
+nxos1 = {
+    'device_type': 'cisco_nxos',
+    'host': 'nxos1.lasthop.io',
+    'username': my_user,
+    'password': my_pass
+    }
+
+nxos2 = {
+    'device_type': 'cisco_nxos',
+    'host': 'nxos2.lasthop.io',
+    'username': my_user,
+    'password': my_pass,
+    'global_delay_factor': 2
+    }
+
+
+net_connect = ConnectHandler(**nxos2)
+
+output = net_connect.send_command('show lldp neighbors detail')
+
+net_connect.disconnect()
+
+print()
+print(output)
+print()
