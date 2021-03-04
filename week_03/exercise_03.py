eba724020f05750f532c455b601bdc719b7db5fe
John Meyers <46431837+jmmey@users.noreply.github.com>
2021-03-04 10:08:02 -0800

    Finally finished

--- week_03/exercise_03.py
+++ week_03/exercise_03.py
@@ -4,6 +4,21 @@ import json
 from pprint import pprint
 
 JSON_FILE = 'napalm_data.json'
+with open(JSON_FILE, 'r') as f:
+    nxos_data = json.load(f)
 
-with open('JSON_FILE', 'r') as f:
+ipv4_data = []
+ipv6_data = []
 
+for intf, ipaddr_dict in nxos_data.items():
+    for ipv4_or_ipv6, addr_info in ipaddr_dict.items():
+        for ip_addr, prefix_dict in addr_info.items():
+            prefix_length = prefix_dict['prefix_length']
+            if ipv4_or_ipv6 == 'ipv4':
+                ipv4_data.append(f'{ip_addr}/{prefix_length}')
+            elif ipv4_or_ipv6 == 'ipv6':
+                ipv6_data.append(f'{ip_addr}/{prefix_length}')
+
+print(f'\n IPv4 Addresses: {ipv4_data}')
+print()
+print(f'\n IPv6 Addresses: {ipv6_data}')
