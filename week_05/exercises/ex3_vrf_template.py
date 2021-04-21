b97f4e8bcf278b358a24e5770c16a438c95e7fe0
John Meyers <46431837+jmmey@users.noreply.github.com>
2021-04-21 08:30:21 -0700

    Finished exercise 3

---
+++ week_05/exercises/ex3_vrf_template.py
@@ -0,0 +1,19 @@
+from jinja2 import FileSystemLoader, StrictUndefined
+from jinja2.environment import Environment
+
+env = Environment(undefined=StrictUndefined)
+env.loader = FileSystemLoader('./templates/exercise3')
+
+j2_vars = {
+    'vrf_name': 'blue',
+    'rd_number': '100:1',
+    'ipv4_af': True, 
+    'ipv6_af': True, 
+}
+
+template_file = 'vrf_template.j2'
+template = env.get_template(template_file)
+output = template.render(**j2_vars)
+print()
+print(output)
+print()
