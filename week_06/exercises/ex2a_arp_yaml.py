9b66e880f1889bb7a994392826360505902c0762
John Meyers <46431837+jmmey@users.noreply.github.com>
2021-04-26 19:12:22 -0700

    Working per the exercise instructions

---
+++ week_06/exercises/ex2a_arp_yaml.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os
+import pyeapi
+import yaml
+from getpass import getpass
+
+
+username = os.getenv("PYNET_USER") if os.getenv("PYNET_USER") else input('Username: ') 
+password = os.getenv("PYNET_PASSWORD") if os.getenv("PYNET_PASSWORD") else getpass('Password: ')
+
+with open('inventory.yml', 'r') as f:
+    inventory = yaml.load(f)
+
+inventory = inventory['arista8']
+inventory['username'] = username
+inventory['password'] = password
+
+connection = pyeapi.client.connect(**inventory)
+
+device = pyeapi.client.Node(connection)
+output = device.enable("show ip arp")
+ip_entries = output[0]['result']['ipV4Neighbors']
+
+print('IPv4 Address | MAC Address')
+print('-' * 28)
+
+for i in ip_entries:
+    print(f"{i['address']} | {i['hwAddress']}") 
